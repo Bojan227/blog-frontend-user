057e4be7cf9460a71230b1a30e329fda149801a0
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-09-08 13:52:52 +0200

    Add custom sign up hooks for the users

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+export const useSignup = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [message, setMessage] = useState('');
+
+  const signup = async (username, password) => {
+    setIsLoading(true);
+    const res = await fetch('/user/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const json = await res.json();
+
+    if (res.ok) {
+      setMessage(json.message);
+      setError('');
+    } else {
+      setError(json.error);
+      setMessage('');
+    }
+    setIsLoading(false);
+  };
+
+  return { signup, isLoading, error, message };
+};
