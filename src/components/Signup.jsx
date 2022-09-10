7af3fd661e96b95a59829b8e6cb42cdc6567c73b
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-09-10 14:11:52 +0200

    Add Signup component

---
+++ src/components/Signup.jsx
@@ -0,0 +1,42 @@
+import {useState} from "react";
+import {Link} from 'react-router-dom'
+import { useSignup } from "../hooks/useSignup";
+
+
+export default function Signup(){
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const {signup, error, isLoading, message} = useSignup()
+
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault()
+        await signup(username, password)
+    }
+
+
+    return (
+        <div className="signup-container">
+            <h1>Get Started</h1>
+            <h3>Already have an account? <span><Link to='login'>Sign In</Link></span></h3>
+
+            <form onSubmit={handleSubmit} style={{backgroundColor: 'white'}}>
+                <label htmlFor="username">Username</label>
+                <input onChange={(e)=>setUsername(e.target.value)} value={username} />
+
+                <label htmlFor="password">Password</label>
+                <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password} />
+
+                <button disabled={isLoading}>Sign Up</button>
+            </form>
+
+
+           <h5>{error}</h5>
+           <h5>{message}</h5>
+
+        </div>
+        
+    )
+
+
+}
