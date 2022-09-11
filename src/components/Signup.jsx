744d8186b53d5daf5fa8f6f2f62a1eaeddf27837
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-09-11 12:29:46 +0200

    Format files with prettier

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,42 +1,43 @@
-import {useState} from "react";
-import {Link} from 'react-router-dom'
-import { useSignup } from "../hooks/useSignup";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useSignup } from '../hooks/useSignup';
 
+export default function Signup() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const { signup, error, isLoading, message } = useSignup();
 
-export default function Signup(){
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const {signup, error, isLoading, message} = useSignup()
+  const handleSubmit = async e => {
+    e.preventDefault();
+    await signup(username, password);
+  };
 
+  return (
+    <div className="signup-container">
+      <h1>Get Started</h1>
+      <h3>
+        Already have an account?{' '}
+        <span>
+          <Link to="login">Sign In</Link>
+        </span>
+      </h3>
 
-    const handleSubmit = async (e) =>{
-        e.preventDefault()
-        await signup(username, password)
-    }
+      <form onSubmit={handleSubmit} style={{ backgroundColor: 'white' }}>
+        <label htmlFor="username">Username</label>
+        <input onChange={e => setUsername(e.target.value)} value={username} />
 
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+        />
 
-    return (
-        <div className="signup-container">
-            <h1>Get Started</h1>
-            <h3>Already have an account? <span><Link to='login'>Sign In</Link></span></h3>
+        <button disabled={isLoading}>Sign Up</button>
+      </form>
 
-            <form onSubmit={handleSubmit} style={{backgroundColor: 'white'}}>
-                <label htmlFor="username">Username</label>
-                <input onChange={(e)=>setUsername(e.target.value)} value={username} />
-
-                <label htmlFor="password">Password</label>
-                <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password} />
-
-                <button disabled={isLoading}>Sign Up</button>
-            </form>
-
-
-           <h5>{error}</h5>
-           <h5>{message}</h5>
-
-        </div>
-        
-    )
-
-
+      <h5>{error}</h5>
+      <h5>{message}</h5>
+    </div>
+  );
 }
