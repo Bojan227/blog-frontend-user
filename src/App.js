6c7bac0b04424c8eeef6070d30af27a0bf349859
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-09-14 16:33:46 +0200

    Add main App file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import { EditPost } from './components/EditPost';
+import Nav from './components/Nav';
+import { PostDetails } from './components/PostDetails';
+import { useUserContext } from './hooks/useUserContext';
+import { PostForm } from './components/PostForm';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
+  const { user } = useUserContext();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <BrowserRouter>
+        {user && <Nav />}
+        <Routes>
+          <Route path="/" element={user ? <Home /> : <Signup />} />
+          <Route
+            path="login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+
+          <Route path="/create-post" element={<PostForm />} />
+          <Route path="/details/:postId" element={<PostDetails />} />
+          <Route path="/edit/:id" element={<EditPost />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
